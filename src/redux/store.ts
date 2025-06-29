f3b19ca2e4ba7a002e234ab5e65549c61e553e77
SmolKate <zdobnovaea@gmail.com>
2025-06-29 16:51:23 +0300

    chore: add redux devtool

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from "redux";
 import { contactsReducer } from "./contactsReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 export const store = createStore(
   combineReducers({
     contacts: contactsReducer,
-  })
+  }),
+  composeWithDevTools()
 );
 
 export type RootState = ReturnType<typeof store.getState>;
